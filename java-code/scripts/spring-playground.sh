88ec6861177720c59ea6a3de69f43a9e328256f4
Csaba Sarkadi <sarkadicsa@tutanota.de>
2021-09-12 18:37:43 +0200

    fixup! fixup! sppifo: port runner to command line interface, use parallel simulation

--- java-code/scripts/spring-playground.sh
+++ java-code/scripts/spring-playground.sh
@@ -6,9 +6,9 @@ PUPD="./projects/sppifo/runs/sppifo_analysis/alternative_distributions/exponenti
 GREEDY="./projects/sppifo/runs/sppifo_analysis/alternative_distributions/exponential/Greedy.properties"
 
 spring_generate() {
-    local sensitivities=(0.2 0.4 0.6 0.8 1.0)
+    local sensitivities=(0.2 0.4 0.6 0.8 1.0 1.5 2.0)
     local alphas=(0.2 0.4 0.6 0.8 1.0)
-    local sample_sizes=(1000 10000 100000)
+    local sample_sizes=(100 1000 10000 100000)
 
     for s in "${sensitivities[@]}"; do
         for a in "${alphas[@]}"; do
@@ -18,7 +18,7 @@ spring_generate() {
                     spring_alpha="${a}" \
                     spring_sensitivity="${s}" \
                     spring_sample_interval="${ss}" \
-                    run_folder_name="${PUPD%%.properties}-spring-a=${a}-s=${s}-ss=${ss}"
+                    run_folder_name="$(basename ${PUPD%%.properties})-spring-a=${a}-s=${s}-ss=${ss}"
             done
         done
     done
