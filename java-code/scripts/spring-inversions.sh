3f29ab04ccb230bf7e61615c987d35acd1a91d26
Csaba Sarkadi <sarkadicsa@tutanota.de>
2021-09-20 22:31:31 +0200

    sppifo: spring-heuristic: update inversion testcase generation script

--- java-code/scripts/spring-inversions.sh
+++ java-code/scripts/spring-inversions.sh
@@ -6,19 +6,21 @@ PUPD="./projects/sppifo/runs/sppifo_analysis/alternative_distributions/exponenti
 GREEDY="./projects/sppifo/runs/sppifo_analysis/alternative_distributions/exponential/Greedy.properties"
 
 spring_generate() {
-    local sensitivities=(0.2 0.4 0.6 0.8 1.0 1.5 2.0)
-    local alphas=(0.2 0.4 0.6 0.8 1.0)
-    local sample_sizes=(100 1000 10000 100000)
+    local sensitivities=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 2.0 5.0 10.0 20.0 50.0 100.0)
+    local alphas=(0.2 0.4 0.6 0.8)
+    local sample_modes=(COUNT_PACKETS COUNT_INVERSIONS)
 
     for s in "${sensitivities[@]}"; do
         for a in "${alphas[@]}"; do
-            for ss in "${sample_sizes[@]}"; do
+            for sm in "${sample_modes[@]}"; do
                 echo "${NETBENCH[@]}" "${PUPD}" \
                     output_port_step_size=spring \
                     spring_alpha="${a}" \
                     spring_sensitivity="${s}" \
-                    spring_sample_interval="${ss}" \
-                    run_folder_name="$(basename ${PUPD%%.properties})-spring-a=${a}-s=${s}-ss=${ss}"
+                    spring_sample_mode="${sm}" \
+                    run_folder_name="$(basename ${PUPD%%.properties})-spring-a=${a}-s=${s}-sm=${sm}" \
+                    enable_rank_mapping=true \
+                    enable_queue_bound_tracking=true
             done
         done
     done
