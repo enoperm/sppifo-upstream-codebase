aaa46a74ace2a0e7c226f7086725da3514657d03
Csaba Sarkadi <sarkadicsa@tutanota.de>
2021-09-25 18:16:15 +0200

    sppifo: scripts: enable bounds tracking for PUPD in spring inversion case generator

--- java-code/scripts/spring-inversions.sh
+++ java-code/scripts/spring-inversions.sh
@@ -3,34 +3,32 @@
 NETBENCH="${NETBENCH:?unset}"
 
 PUPD="./projects/sppifo/runs/sppifo_analysis/alternative_distributions/exponential/SPPIFO.properties"
-GREEDY="./projects/sppifo/runs/sppifo_analysis/alternative_distributions/exponential/Greedy.properties"
+
+COMMON_ARGS=("enable_rank_mapping=true" "enable_queue_bound_tracking=true" "sppifo_queuebound_log_interval=100")
 
 spring_generate() {
-    local sensitivities=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 2.0 5.0 10.0 20.0 50.0 100.0)
-    local alphas=(0.2 0.4 0.6 0.8)
-    local sample_modes=(COUNT_PACKETS COUNT_INVERSIONS)
+    local sensitivities=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 2.0 5.0 10.0 20.0)
+    local alphas=(0.0 0.1 0.2 0.4 0.6 0.8 0.9 1.0)
+    local sample_modes=(COUNT_PACKETS COUNT_INVERSIONS SUM_RANKS SUM_INVERSION_MAGNITUDE)
 
     for s in "${sensitivities[@]}"; do
         for a in "${alphas[@]}"; do
             for sm in "${sample_modes[@]}"; do
+                # Uses PUPD config as a base to keep base parameters the same.
                 echo "${NETBENCH[@]}" "${PUPD}" \
+                    "${COMMON_ARGS[@]}" \
                     output_port_step_size=spring \
                     spring_alpha="${a}" \
                     spring_sensitivity="${s}" \
                     spring_sample_mode="${sm}" \
-                    run_folder_name="$(basename ${PUPD%%.properties})-spring-a=${a}-s=${s}-sm=${sm}" \
-                    enable_rank_mapping=true \
-                    enable_queue_bound_tracking=true
+                    run_folder_name="$(basename ${PUPD%%.properties})-spring-a=${a}-s=${s}-sm=${sm}"
             done
         done
     done
 }
 
 # PUPD
-echo "${NETBENCH[@]}" "${PUPD}"
-
-# Greedy
-echo "${NETBENCH[@]}" "${GREEDY}"
+echo "${NETBENCH[@]}" "${PUPD}" "${COMMON_ARGS[@]}"
 
 # Spring-like heuristic with various parameters
 spring_generate
